7d72fc4137eeb7582182aef2f6bccca23e4a6edc
Protocol789 <44654683+Protocol789@users.noreply.github.com>
2024-01-12 08:12:39 +0000

    Update bootstrap.sh

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -1,24 +1,26 @@
 #/bin/bash
 
-ver=0.2
-rel=v0.2.tar.gz
+# Get latest tag
+latesttag=$(curl -s "https://api.github.com/repos/Protocol789/homeassistant/tags" | jq -r '.[0].name')
+dlURL="https://github.com/Protocol789/homeassistant/archive/refs/tags/$latesttag.tar.gz"
+dlFilename="$latesttag.tar.gz"
 
 # Get release
 echo "Grabbing release from Github"
-wget "https://github.com/Protocol789/homeassistant/archive/refs/tags/$rel"
+wget "$dlURL"
 
 echo "Decrompress archive"
-tar -xvzf "$rel"
+tar -xvzf "$dlFilename"
 
 # Sort folders
 echo "Sorting folders out"
 mkdir func
-cp ./homeassistant-$ver/func/* func/
-cp ./homeassistant-$ver/Airtel_GetBalance.sh ./
+cp ./homeassistant-$latesttag/func/* func/
+cp ./homeassistant-$latesttag/Airtel_GetBalance.sh ./
 
 # Clean up
 echo "Clean up"
-rm "$rel"
-rm -rf homeassistant-$ver/
+rm "$dlFilename"
+rm -rf homeassistant-$latesttag/
 
 echo "--------- DONE -----------"
